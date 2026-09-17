a1e3c86ddd948d34bab2744e71f4c77646658c02
JitBhatiya <jitbhatiya51@gmail.com>
2026-09-17 22:18:03 +0530

    add components

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import {useDispatch} from 'react-redux'
+import {setQuery} from '../redux/features/searchSlice'
+const SearchBar = () => {
+
+    const [text, setText] = useState('')
+
+    const dispatch = useDispatch()
+
+    const submitHandler = (e)=> {
+        dispatch(setQuery(text))
+        e.preventDefault()
+        setText('')
+    }
+  return (
+    <div>
+        <form onSubmit={(e)=> {
+                submitHandler(e)
+        }} className='flex bg-gray-900 p-10 gap-5'>
+            <input
+             value={text}
+             onChange={(e)=> {
+                setText(e.target.value);
+            }} required className='w-full border-2 px-4 py-2 text-xl rounded outline-none' type='text' placeholder='Search anything..'></input>
+            <button className='active:scale-95 border-2 px-4 py-2 text-xl rounded outline-none cursor-pointer'>Search</button>
+        </form>
+    </div>
+  )
+}
+
+export default SearchBar
---
+++ src/components/Tabs.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setActiveTab } from '../redux/features/searchSlice'
+
+const Tabs = () => {
+    const tabs = ['photos', 'videos', 'GIFs']
+
+    const dispatch = useDispatch()
+
+    const activeTab = useSelector((state)=>state.search.activeTab)
+  return (
+    <div className='flex gap-5 p-10'>
+        {tabs.map(function(elem,idx){
+            return <button 
+            className={`${(activeTab==elem?'bg-blue-700':'bg-gray-600')}px-5 py-2 cursor-pointer active:scale-95  rounded uppercase`}
+             key={idx}
+             onClick={()=> {
+                dispatch(setActiveTab(elem))
+             }}
+             >
+                {elem}
+                </button>
+        }     )}
+    </div>
+  )
+}
+
+export default Tabs
